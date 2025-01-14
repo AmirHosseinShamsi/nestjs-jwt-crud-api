fa5fb13e993499be31a59c3200d76ade5c3baa13
AmirHossein Shamsi <shamsiamir2000@gmail.com>
2025-01-14 22:45:26 +0330

    refactor database connection information , using seperate module for database connection and using configModule for accessing .env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { User } from './user/user.entity';
+import { DatabaseModule } from './database/database.module';
 import 'dotenv/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST, // Replace with your PostgreSQL host
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true, // Automatically load entities
-      entities: [User],
-      synchronize: true, // Auto-sync entities with the database (disable in production)
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
+    DatabaseModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -17,93 +17,134 @@ export class UserService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
+
   async findAll(): Promise<UserInterface[]> {
-    return this.userRepository.createQueryBuilder('user').getMany();
+    try {
+      return this.userRepository.createQueryBuilder().getMany();
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async findOne(id: number): Promise<UserInterface | null> {
-    const user = await this.userRepository
-      .createQueryBuilder('user')
-      .where('user.id = :id', { id })
-      .getOne();
-    if (!user) {
-      throw new NotFoundException('the user not found');
+    try {
+      const user = await this.userRepository
+        .createQueryBuilder('user')
+        .where('user.id = :id', { id })
+        .getOne();
+      if (!user) {
+        throw new NotFoundException('the user not found');
+      }
+      return user;
+    } catch (error) {
+      if (error instanceof NotFoundException) throw error;
+      throw new HttpException(
+        'something went wrong',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-    return user;
   }
 
   async createUser(createUserDto: CreateUserDto): Promise<messageInterface> {
-    const createdUser = await this.userRepository
-      .createQueryBuilder()
-      .insert()
-      .into(User)
-      .values(createUserDto)
-      .execute();
-    return {
-      message: `the user with id ${createdUser.identifiers[0].id} created successfully`,
-    };
+    try {
+      const createdUser = await this.userRepository
+        .createQueryBuilder()
+        .insert()
+        .into(User)
+        .values(createUserDto)
+        .execute();
+      return {
+        message: `the user with id ${createdUser.identifiers[0].id} created successfully`,
+      };
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async updateAll(
     id: number,
     updateUserDto: UpdateUserDto,
   ): Promise<messageInterface> {
-    const isUserExists = await this.userRepository
-      .createQueryBuilder('user')
-      .where('user.id = :id', { id })
-      .getOne();
-    if (!isUserExists) {
-      throw new NotFoundException('the use not found');
+    try {
+      const isUserExists = await this.userRepository
+        .createQueryBuilder('user')
+        .where('user.id = :id', { id })
+        .getOne();
+      if (!isUserExists) {
+        throw new NotFoundException('the use not found');
+      }
+      await this.userRepository
+        .createQueryBuilder()
+        .update(User)
+        .set(updateUserDto)
+        .where('id = :id', { id })
+        .execute();
+      return {
+        message: `the user with id ${id} has been updated successfully`,
+      };
+    } catch (error) {
+      if (error instanceof NotFoundException) throw error;
+      throw new HttpException(
+        'something went wrong',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-    const updatedUser: any = await this.userRepository
-      .createQueryBuilder()
-      .update(User)
-      .set(updateUserDto)
-      .where('id = :id', { id })
-      .execute();
-    if (updatedUser.affected === 0) {
-      throw new HttpException('something gone wrong!', HttpStatus.BAD_GATEWAY);
-    }
-    return { message: `the user with id ${id} has been updated successfully` };
   }
 
   async updatePartial(
     id: number,
     partialUpdateUserDto: PartialUpdateUserDto,
   ): Promise<messageInterface> {
-    const isUserExists = await this.userRepository
-      .createQueryBuilder('user')
-      .where('user.id = :id', { id })
-      .getOne();
-    if (!isUserExists) {
-      throw new NotFoundException('the use not found');
+    try {
+      const isUserExists = await this.userRepository
+        .createQueryBuilder('user')
+        .where('user.id = :id', { id })
+        .getOne();
+      if (!isUserExists) {
+        throw new NotFoundException('the use not found');
+      }
+      await this.userRepository
+        .createQueryBuilder()
+        .update(User)
+        .set(partialUpdateUserDto)
+        .where('id = :id', { id })
+        .execute();
+      return {
+        message: `the user with id ${id} has been updated successfully`,
+      };
+    } catch (error) {
+      if (error instanceof NotFoundException) throw error;
+      throw new HttpException(
+        'something went wrong',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-    const updatedUser: any = await this.userRepository
-      .createQueryBuilder()
-      .update(User)
-      .set(partialUpdateUserDto)
-      .where('id = :id', { id })
-      .execute();
-    if (updatedUser.affected === 0) {
-      throw new HttpException('something gone wrong!', HttpStatus.BAD_GATEWAY);
-    }
-    return { message: `the user with id ${id} has been updated successfully` };
   }
 
   async deleteOne(id: number): Promise<messageInterface> {
-    const isUserExists = await this.userRepository
-      .createQueryBuilder('user')
-      .where('user.id = :id', { id })
-      .getOne();
-    if (!isUserExists) {
-      throw new NotFoundException('the use not found');
+    try {
+      const isUserExists = await this.userRepository
+        .createQueryBuilder('user')
+        .where('user.id = :id', { id })
+        .getOne();
+      if (!isUserExists) {
+        throw new NotFoundException('the use not found');
+      }
+      await this.userRepository
+        .createQueryBuilder()
+        .delete()
+        .from(User)
+        .where('id = :id', { id })
+        .execute();
+      return {
+        message: `the user with id ${id} has been removed successfully`,
+      };
+    } catch (error) {
+      if (error instanceof NotFoundException) throw error;
+      throw new HttpException(
+        'something went wrong',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-    await this.userRepository
-      .createQueryBuilder()
-      .delete()
-      .from(User)
-      .where('id = :id', { id })
-      .execute();
-    return { message: `the user with id ${id} has been removed successfully` };
   }
 }
