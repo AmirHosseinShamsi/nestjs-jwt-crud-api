9ea0ae7ff4467c5db39ae234aac005618e480d3e
AmirHossein Shamsi <shamsiamir2000@gmail.com>
2025-01-14 23:50:33 +0330

    the best approach for crud operation after refactor the service of user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,4 @@
-import {
-  HttpException,
-  HttpStatus,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { DeleteResult, Repository } from 'typeorm';
