b5be0607a6111ace312c1b7abb8f7244b81f8900
AmirHossein Shamsi <shamsiamir2000@gmail.com>
2025-01-14 23:49:57 +0330

    the best approach for crud operation after refactor the service of user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import { UserInterface, messageInterface } from './interfaces/user.interface';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -19,132 +19,82 @@ export class UserService {
   ) {}
 
   async findAll(): Promise<UserInterface[]> {
-    try {
-      return this.userRepository.createQueryBuilder().getMany();
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    return this.userRepository.createQueryBuilder().getMany();
   }
 
   async findOne(id: number): Promise<UserInterface | null> {
-    try {
-      const user = await this.userRepository
-        .createQueryBuilder('user')
-        .where('user.id = :id', { id })
-        .getOne();
-      if (!user) {
-        throw new NotFoundException('the user not found');
-      }
-      return user;
-    } catch (error) {
-      if (error instanceof NotFoundException) throw error;
-      throw new HttpException(
-        'something went wrong',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+    const user = await this.userRepository
+      .createQueryBuilder('user')
+      .where('user.id = :id', { id })
+      .getOne();
+    if (!user) {
+      throw new NotFoundException('the user not found');
     }
+    return user;
   }
 
   async createUser(createUserDto: CreateUserDto): Promise<messageInterface> {
-    try {
-      const createdUser = await this.userRepository
-        .createQueryBuilder()
-        .insert()
-        .into(User)
-        .values(createUserDto)
-        .execute();
-      return {
-        message: `the user with id ${createdUser.identifiers[0].id} created successfully`,
-      };
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    const createdUser = await this.userRepository
+      .createQueryBuilder()
+      .insert()
+      .into(User)
+      .values(createUserDto)
+      .execute();
+    return {
+      message: `the user with id ${createdUser.identifiers[0].id} created successfully`,
+    };
   }
 
   async updateAll(
     id: number,
     updateUserDto: UpdateUserDto,
   ): Promise<messageInterface> {
-    try {
-      const isUserExists = await this.userRepository
-        .createQueryBuilder('user')
-        .where('user.id = :id', { id })
-        .getOne();
-      if (!isUserExists) {
-        throw new NotFoundException('the use not found');
-      }
-      await this.userRepository
-        .createQueryBuilder()
-        .update(User)
-        .set(updateUserDto)
-        .where('id = :id', { id })
-        .execute();
-      return {
-        message: `the user with id ${id} has been updated successfully`,
-      };
-    } catch (error) {
-      if (error instanceof NotFoundException) throw error;
-      throw new HttpException(
-        'something went wrong',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+    const updateResult = await this.userRepository
+      .createQueryBuilder()
+      .update(User)
+      .set(updateUserDto)
+      .where('id = :id', { id })
+      .execute();
+
+    if (updateResult.affected === 0) {
+      throw new NotFoundException('The user not found');
     }
+    return {
+      message: `the user with id ${id} has been updated successfully`,
+    };
   }
 
   async updatePartial(
     id: number,
     partialUpdateUserDto: PartialUpdateUserDto,
   ): Promise<messageInterface> {
-    try {
-      const isUserExists = await this.userRepository
-        .createQueryBuilder('user')
-        .where('user.id = :id', { id })
-        .getOne();
-      if (!isUserExists) {
-        throw new NotFoundException('the use not found');
-      }
-      await this.userRepository
-        .createQueryBuilder()
-        .update(User)
-        .set(partialUpdateUserDto)
-        .where('id = :id', { id })
-        .execute();
-      return {
-        message: `the user with id ${id} has been updated successfully`,
-      };
-    } catch (error) {
-      if (error instanceof NotFoundException) throw error;
-      throw new HttpException(
-        'something went wrong',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+    const updateResult = await this.userRepository
+      .createQueryBuilder()
+      .update(User)
+      .set(partialUpdateUserDto)
+      .where('id = :id', { id })
+      .execute();
+
+    if (updateResult.affected === 0) {
+      throw new NotFoundException('The user not found');
     }
+    return {
+      message: `the user with id ${id} has been updated successfully`,
+    };
   }
 
   async deleteOne(id: number): Promise<messageInterface> {
-    try {
-      const isUserExists = await this.userRepository
-        .createQueryBuilder('user')
-        .where('user.id = :id', { id })
-        .getOne();
-      if (!isUserExists) {
-        throw new NotFoundException('the use not found');
-      }
-      await this.userRepository
-        .createQueryBuilder()
-        .delete()
-        .from(User)
-        .where('id = :id', { id })
-        .execute();
-      return {
-        message: `the user with id ${id} has been removed successfully`,
-      };
-    } catch (error) {
-      if (error instanceof NotFoundException) throw error;
-      throw new HttpException(
-        'something went wrong',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+    const deletedResult: DeleteResult = await this.userRepository
+      .createQueryBuilder()
+      .delete()
+      .from(User)
+      .where('id = :id', { id })
+      .execute();
+    if (deletedResult.affected === 0) {
+      throw new NotFoundException('The user not found');
     }
+    return {
+      message: `the user with id ${id} has been removed successfully`,
+    };
   }
 }
